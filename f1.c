877f2c24337f4b769a9feea71be30662938cb50f
amuleek <amuleeksidhu96@gmail.com>
2019-03-18 10:25:12 +0530

    sixth commit

--- f1.c
+++ f1.c
@@ -6,10 +6,11 @@ int main()
     // printf() dislpays the formatted output 
     printf("Enter an integer: ");  
     
-    // scanf() reads the formatted input and stores them
-    scanf("%d", &number);  
+    // scanf() reads the formatted input and  it stores them
+    scanf("%d", &number );  
     
     // printf() displays the formatted output
     printf("You entered: %d", number);
     return 0;
+
 }
