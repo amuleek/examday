15f9ee1c734bc7bc86dba9ac9288029a09ed8efb
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-04-26 09:45:33 +0530

    Update sizeofloperators.c

--- sizeofloperators.c
+++ sizeofloperators.c
@@ -12,3 +12,11 @@ int main(int argc, char** argv)
     printf("INT_MIN     :   %d\n", INT_MIN);
     printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
     printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
+    printf("SCHAR_MAX   :   %d\n", SCHAR_MAX);
+    printf("SCHAR_MIN   :   %d\n", SCHAR_MIN);
+    printf("SHRT_MAX    :   %d\n", SHRT_MAX);
+    printf("SHRT_MIN    :   %d\n", SHRT_MIN);
+    printf("UCHAR_MAX   :   %d\n", UCHAR_MAX);
+    printf("UINT_MAX    :   %u\n", (unsigned int) UINT_MAX);
+    printf("ULONG_MAX   :   %lu\n", (unsigned long) ULONG_MAX);
+    printf("USHRT_MAX   :   %d\n", (unsigned short) USHRT_MAX);
