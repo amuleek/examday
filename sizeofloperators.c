d1ad4827e37a61676269a4a315b9a8f105629dc0
Amuleek Sidhu <46739438+amuleek@users.noreply.github.com>
2020-04-26 09:45:10 +0530

    Update sizeofloperators.c

--- sizeofloperators.c
+++ sizeofloperators.c
@@ -1,4 +1,14 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<limits.h>
-#include<float.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <float.h>
+int main(int argc, char** argv) 
+{
+
+    printf("CHAR_BIT    :   %d\n", CHAR_BIT);
+    printf("CHAR_MAX    :   %d\n", CHAR_MAX);
+    printf("CHAR_MIN    :   %d\n", CHAR_MIN);
+    printf("INT_MAX     :   %d\n", INT_MAX);
+    printf("INT_MIN     :   %d\n", INT_MIN);
+    printf("LONG_MAX    :   %ld\n", (long) LONG_MAX);
+    printf("LONG_MIN    :   %ld\n", (long) LONG_MIN);
